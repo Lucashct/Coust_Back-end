0a433f5279ed1351f7dabded7e303875af6213d2
Lucas Torres <lucascms11@gmail.com>
2024-07-07 19:12:50 -0300

    [CHANGE] Return origin to localhost

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: 'https://coust.netlify.app',
+  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
