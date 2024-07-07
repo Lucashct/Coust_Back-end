fb014540c0492e3844e0c4d8b7d285cd4527d1dd
Lucas Torres <lucascms11@gmail.com>
2024-07-07 18:51:00 -0300

    [CHANGE] Change origin cors

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://coust-back-end.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
