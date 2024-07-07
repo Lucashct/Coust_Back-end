246efb730fc01485b7131b06a3f9cc398707eec6
Lucas Henrique C. Torres <62104459+Lucashct@users.noreply.github.com>
2024-07-07 19:07:31 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: 'https://coust-back-end.onrender.com',
+  origin: 'https://coust.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
