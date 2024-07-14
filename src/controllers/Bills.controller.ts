8b1007c4c880976671e8a0c2553fb52350d6eae1
Lucas Torres <lucascms11@gmail.com>
2024-07-14 20:56:53 -0300

    [FEATURE] Feature to add bills and list on the client.

--- src/controllers/Bills.controller.ts
+++ src/controllers/Bills.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { createBill } from "../repositorys/bill.repository";
+import { createEntrie } from "../repositorys/entrie.repository";
+import { ResponseObject } from "../util/ResponseObject";
+import { StatusResponse } from "../enums/StatusResponse";
 
 export class BillsController {
 	public async create(req: Request, res: Response) {
@@ -10,16 +13,32 @@ export class BillsController {
 				ammount,
 				dueDate,
 				card,
-        entriesQt
+        entriesQt,
+				user
 			} = req.body;
+			const ammountToFloat = parseFloat(ammount);
+			const entriesQtToFloat = parseFloat(entriesQt);
 
-      const createdBill = await createBill({ title, type, ammount, dueDate: new Date(dueDate) });
+      const createdBill = await createBill({ title, type, ammount: ammountToFloat, dueDate: new Date(dueDate), idCard: card ? card.id : undefined, idUser: user.id });
 
-      if(card) {
-        
-      }
+			const listOfCreatedEntries = [];
+			
+      for(let i = 0; i < entriesQt; i++) {
+				const dateDueDate = new Date(dueDate);
+
+				const createdEntrie = await createEntrie({ value: parseFloat((ammountToFloat / entriesQtToFloat).toFixed(2)), dueDate: new Date(dateDueDate.setMonth(dateDueDate.getMonth() + i)), idBill: createdBill.id, idUser: user.id });
+
+				listOfCreatedEntries.push({ ...createdEntrie });
+			}
+      
+			return res.status(201)
+			.json(new ResponseObject( StatusResponse.SUCCESS, 'Conta criada com sucesso!', null, 
+				{ entries: listOfCreatedEntries, bill: createdBill  }
+			))
 		} catch(error) {
+			console.log(error);
 
+			return res.status(500).json(new ResponseObject(StatusResponse.ERROR, 'Ocorreu um erro ao comunicar-se com o servidor.', null, null))
 		}
 	}
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { validSession } from "./middleware/ValidSession";
 import LoginController from "./controllers/Login.controller";
 import UserController from "./controllers/User.controller";
 import { CardsController } from "./controllers/Cards.controller";
+import { BillsController } from "./controllers/Bills.controller";
 
 const routes = Router();
 
@@ -18,5 +19,7 @@ routes.get('/logout', new LoginController().logOut);
 
 routes.post('/cards/create', new CardsController().create);
 routes.post('/cards/remove', new CardsController().remove);
+
+routes.post('/bills/create', new BillsController().create);
 
 export default routes;
